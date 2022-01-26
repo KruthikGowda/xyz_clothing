b5fb1df674084bef4a31e18c47796b17f910da3a
Kruthik Gowda <prkruthikgowda@gmail.com>
2022-01-27 02:48:24 +0530

    Half point

---
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import { selectCollectionsForPreview } from "../../redux/shop/shop.selector";
+import PreviewCollection from "../preveiw-collection/preview-collection.component";
+
+import "./collections-overview.styles.scss";
+
+const CollectionsOverview = ({ collections }) => (
+  <div className="collections-overview">
+    {collections.map(({ id, ...otherCollectionProps }) => (
+      <PreviewCollection key={id} {...otherCollectionProps} />
+    ))}
+  </div>
+);
+
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollectionsForPreview,
+});
+
+export default connect(mapStateToProps)(CollectionsOverview);
